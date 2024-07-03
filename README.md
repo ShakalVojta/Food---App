232f5115ec273a4b7c06623c7a7e921fdb6734f0
Vojtěch Týnavský <vojtech.tynavsky@seznam.cz>
2024-07-04 00:09:33 +0200

    Set up main page and prepare for cart components

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Food---App
 My Full-stack project, where I try to create Food delivery applications.
+I start with frontend, where I use React JS
+For DB I use MongoDB
+For backend I use Node JS, ExpressJS and Stripe
---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Home from "./pages/Home/Home.jsx";
+import Cart from "./pages/Cart/Cart.jsx";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import { Route, Routes } from "react-router-dom";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import LoginPopup from "./components/LoginPopup/LoginPopup.jsx";
+
+const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false);
+
+  return (
+    <>
+    {showLogin ? <LoginPopup setShowLogin={setShowLogin}/> : <></>}
+      <div className="app">
+        <NavBar setShowLogin={setShowLogin}/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
