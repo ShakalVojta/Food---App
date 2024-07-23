9a8853014a9395d992e4fece98cfceee27862b97
Vojtěch Týnavský <vojtech.tynavsky@seznam.cz>
2024-07-23 14:23:55 +0200

    Setup login and registration form and create shop cart functionality

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@
  import cors from "cors"
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config';
+import cartRouter from "./routes/cartRoute.js";
 
 
  //app config
@@ -18,6 +21,8 @@ connectDB();
 // API endpoints
 app.use("/api/food", foodRouter)
 app.use("/images", express.static("uploads"))
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
 
 app.get("/", (req, res)=> {
     res.send("API WORKING")
